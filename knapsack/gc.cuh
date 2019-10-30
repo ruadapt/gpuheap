f13bb0c6cda075da5b77854441e4df11b6bc8b69
Chenyh64 <chenyh64@gmail.com>
2019-10-30 18:30:31 -0400

    cleanup knapsack implementation

---
+++ knapsack/gc.cuh
@@ -0,0 +1,142 @@
+#ifndef GC_CUH
+#define GC_CUH
+
+#include "heap.cuh"
+#include "datastructure.hpp"
+
+#include "knapsackKernel.cuh"
+__global__ void garbageCollection(Heap<KnapsackItem> *heap, int batchSize, int batchCount,
+                        int *weight, int *benefit, float *benefitPerWeight,
+                        int capacity, int inputSize,
+                        int *max_benefit, int k,
+                        KnapsackItem *insItems, int *insSize)
+{
+    KnapsackItem dummy_item(INIT_LIMITS, 0, 0, 0);
+    // TODO now only support batchsize == blockSize
+    /*handle parital batch first*/
+    if (blockIdx.x == 0) {
+        for (int i = threadIdx.x; i < *heap->partialBufferSize; i += blockDim.x) {
+            int oldBenefit = -(heap->heapItems[i]).first;
+            int oldWeight = (heap->heapItems[i]).second;
+            short oldIndex = (heap->heapItems[i]).third;
+
+            int _bound = ComputeBound(oldBenefit, oldWeight, oldIndex, inputSize, capacity, weight, benefit);
+            if (oldBenefit + _bound > *max_benefit) {
+                int index = atomicAdd(insSize, 1);
+                insItems[index] = heap->heapItems[i];
+            }
+            heap->heapItems[i] = dummy_item;
+        }
+        __syncthreads();
+        if (threadIdx.x == 0) {
+            *heap->partialBufferSize = 0;
+        }
+        __syncthreads();
+    }
+    int startBatchIdx = batchCount - k >= 0 ? batchCount - k + 1 : 1;
+    for (int batchIndex = startBatchIdx + blockIdx.x; batchIndex <= batchCount; batchIndex += gridDim.x) {
+        if(threadIdx.x == 0) {
+            heap->status[batchIndex] = AVAIL;
+        }
+
+        int i = batchIndex * batchSize + threadIdx.x;
+        int oldBenefit = -(heap->heapItems[i]).first;
+        int oldWeight = (heap->heapItems[i]).second;
+        short oldIndex = (heap->heapItems[i]).third;
+
+        int _bound = ComputeBound(oldBenefit, oldWeight, oldIndex, inputSize, capacity, weight, benefit);
+		if (oldBenefit + _bound > *max_benefit) {
+            int index = atomicAdd(insSize, 1);
+            insItems[index] = heap->heapItems[i];
+		}
+        heap->heapItems[i] = dummy_item;
+    }
+
+    if(!threadIdx.x && !blockIdx.x){
+        *heap->batchCount = startBatchIdx - 1;
+    }
+    __syncthreads();
+
+}
+
+__global__ void HeapInsert(Heap<KnapsackItem> *heap, KnapsackItem *insItems, int *insSize, int batchSize){	
+	if(!(*insSize))
+		return;
+	int batchNeed = (*insSize + batchSize - 1) / batchSize;
+    for (int i = blockIdx.x; i < batchNeed; i += gridDim.x) {
+        int size = min(batchSize, *insSize - i * batchSize);
+        heap->insertion(insItems + i * batchSize,
+                        size, 0);
+        __syncthreads();
+    }
+	
+}
+
+void invalidFilter(Heap<KnapsackItem> &heap, Heap<KnapsackItem> *d_heap, int batchSize,
+                    int *weight, int *benefit, float *benefitPerWeight,
+                    int capacity, int inputSize,
+                    int *max_benefit, int k = 10240)
+{
+
+    KnapsackItem *insItems;
+    int *insSize;
+    cudaMalloc((void **)&insItems, k * batchSize * sizeof(KnapsackItem));
+    cudaMalloc((void **)&insSize, sizeof(int));
+    cudaMemset(insSize, 0, sizeof(int));
+
+    int blockNum = 32;
+    int blockSize = batchSize;
+    int batchCount = 0;
+    cudaMemcpy(&batchCount, heap.batchCount, sizeof(int), cudaMemcpyDeviceToHost);
+
+    size_t smemOffset = sizeof(KnapsackItem) * batchSize * 3 +
+                        sizeof(int) * 2 +
+                        sizeof(KnapsackItem) +
+                        5 * batchSize * sizeof(KnapsackItem);
+
+    garbageCollection<<<blockNum, blockSize>>>(d_heap, batchSize, batchCount,
+                                                weight, benefit, benefitPerWeight,
+                                                capacity, inputSize,
+                                                max_benefit, k,
+                                                insItems, insSize);
+    cudaDeviceSynchronize();
+
+    HeapInsert<<<blockNum, blockSize, smemOffset>>>(d_heap, insItems, insSize, batchSize);
+    cudaDeviceSynchronize();
+
+    cudaFree(insItems); insItems = NULL;
+    cudaFree(insSize); insSize = NULL;
+
+}
+
+/*void invalidFilter2Heap(Heap<KnapsackItem> heap1, Heap<KnapsackItem> heap2, */
+                        /*Heap<KnapsackItem> *d_heap1, Heap<KnapsackItem> *d_heap2, int batchSize,*/
+                        /*int *weight, int *benefit, float *benefitPerWeight,*/
+                        /*int capacity, int inputSize, int *max_benefit,*/
+                        /*int expandFlag, int gcThreshold, int k)*/
+/*{*/
+    /*if (expandFlag == 0) {*/
+        /*int batchCount = heap1.nodeCount();*/
+        /*if (batchCount > gcThreshold) {*/
+            /*cout << "gc..." << batchCount;*/
+            /*invalidFilter(heap1, d_heap1, batchSize, batchCount,*/
+                          /*weight, benefit, benefitPerWeight,*/
+                          /*capacity, inputSize, max_benefit, 1024000);*/
+            /*cout << heap2.nodeCount();*/
+        /*}*/
+    /*}*/
+    /*else if (expandFlag == 1) {*/
+        /*int batchCount = heap2.nodeCount();*/
+        /*if (batchCount > gcThreshold) {*/
+            /*cout << "gc..." << batchCount;*/
+            /*invalidFilter(heap2, d_heap2, batchSize, batchCount,*/
+                          /*weight, benefit, benefitPerWeight,*/
+                          /*capacity, inputSize, max_benefit, 1024000);*/
+            /*cout << heap2.nodeCount();*/
+        /*}*/
+    /*}*/
+/*}*/
+
+
+
+#endif
---
+++ knapsack/main.cu
@@ -0,0 +1,102 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#include "models.cuh"
+#include "util.cuh"
+#include "datastructure.hpp"
+
+using namespace std;
+
+bool cmp(KnapsackItem a, KnapsackItem b)
+{
+    double r1 = (double)a.first / (double)a.second;
+    double r2 = (double)b.first / (double)b.second;
+    return r1 > r2;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 11) {
+        cout << "./knapsack [dataset] [batchnum] [batchsize] [blocknum] [blocksize] \
+            [model] [delAllowed] [gcThreshold] [expandThreshold] [endingBlockNum]\n";
+        return 1;
+    }
+    ifstream inputFile;
+
+    int batchNum = atoi(argv[2]);
+    int batchSize = atoi(argv[3]);
+    int blockNum = atoi(argv[4]);
+    int blockSize = atoi(argv[5]);
+    int gc_threshold = atoi(argv[6]);
+    /*int model = atoi(argv[6]);*/
+    /*int delAllowed = atoi(argv[7]);*/
+    /*int gcThreshold = atoi(argv[8]);*/
+    /*int expandThreshold = atoi(argv[9]);*/
+    /*int endingBlockNum = atoi(argv[10]);*/
+
+    inputFile.open(argv[1]);
+
+    int capacity, inputSize;
+    inputFile >> inputSize >> capacity;
+
+    int *weight = new int[inputSize];
+    int *benefit = new int[inputSize];
+    float *benefitPerWeight = new float[inputSize];
+
+    for (int i = 0; i < inputSize; i++) {
+        inputFile >> benefit[i] >> weight[i];
+        benefitPerWeight[i] = (float)benefit[i] / (float)weight[i];
+    }
+
+    inputFile.close();
+
+	// Sort items by ppw
+	KnapsackItem *items = new KnapsackItem[inputSize];
+	for (int i = 0; i < inputSize; i++){
+		items[i] = KnapsackItem(benefit[i], weight[i], 0, 0);
+	}
+	sort(items, items + inputSize, cmp);
+	for (int i = 0; i < inputSize; i++){
+		benefit[i] = items[i].first;
+		weight[i] = items[i].second;
+		benefitPerWeight[i] = (float)(benefit[i]) / (float)(weight[i]);
+
+	}
+	delete[]items;
+
+	int *d_weight, *d_benefit;
+    float *d_benefitPerWeight;
+    cudaMalloc((void **)&d_weight, sizeof(int) * inputSize);
+    cudaMalloc((void **)&d_benefit, sizeof(int) * inputSize); 
+    cudaMalloc((void **)&d_benefitPerWeight, sizeof(float) * inputSize);
+    cudaMemcpy(d_weight, weight, sizeof(int) * inputSize, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_benefit, benefit, sizeof(int) * inputSize, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_benefitPerWeight, benefitPerWeight, sizeof(float) * inputSize, cudaMemcpyHostToDevice);
+
+    int max_benefit = 0;
+    int *d_max_benefit;
+    cudaMalloc((void **)&d_max_benefit, sizeof(int));
+    cudaMemcpy(d_max_benefit, &max_benefit, sizeof(int), cudaMemcpyHostToDevice);
+
+    oneheap(d_weight, d_benefit, d_benefitPerWeight,
+            d_max_benefit, capacity, inputSize,
+            batchNum, batchSize, blockNum, blockSize,
+            gc_threshold);
+
+    cudaMemcpy(&max_benefit, d_max_benefit, sizeof(int), cudaMemcpyDeviceToHost);
+
+    cout << max_benefit << endl;
+
+    delete[] weight; weight = NULL;
+    delete[] benefit; benefit = NULL;
+    delete[] benefitPerWeight; benefitPerWeight = NULL;
+    cudaFree(d_weight);
+    cudaFree(d_benefit);
+    cudaFree(d_benefitPerWeight);
+    cudaFree(d_max_benefit);
+
+    return 0;
+
+}
