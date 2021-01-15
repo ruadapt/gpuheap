c2fbc54b8b52de1bfb1cc0d99274e4018ff9ab91
Chenyh64 <chenyh64@gmail.com>
2021-01-15 14:07:51 -0500

    Update TOD & sssp test script

--- artifacts/sssp/test.sh
+++ artifacts/sssp/test.sh
@@ -4,11 +4,11 @@ repeat=1
 #nthreads=40
 
 for ((i=0;i<${repeat};i++)); do
-    ./ssspT com-LiveJournal.out 
-    ./ssspT mawi_201512020000.out 
-    ./ssspT hollywood-2009.out
+    ./ssspT datasets/com-LiveJournal.out 
+    ./ssspT datasets/mawi_201512020000.out 
+    ./ssspT datasets/hollywood-2009.out
 
-    ./ssspB com-LiveJournal.out 
-    ./ssspB mawi_201512020000.out 
-    ./ssspB hollywood-2009.out
+    ./ssspB datasets/com-LiveJournal.out 
+    ./ssspB datasets/mawi_201512020000.out 
+    ./ssspB datasets/hollywood-2009.out
 done
